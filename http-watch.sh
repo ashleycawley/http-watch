4c542d4bf1a568568ddaac58bb409a41eefc6ee3
Ashley Cawley <ash@ashleycawley.co.uk>
2018-11-28 10:17:46 -0500

    init commit

---
+++ http-watch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Variables
+
+# Functions
+
+# Script
+whiptail --title "http-watch" --msgbox "This wizard will help you implement monitoring of a URL and if it goes offline it can alert you and take action (restart your web server) for you." 8 78
